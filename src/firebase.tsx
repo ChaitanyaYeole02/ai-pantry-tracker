26afba5aed4234b78d8fb2d025ee0d020ba6ac23
chaitanya-yeole <cyeole@outlook.com>
2024-08-06 17:01:26 -0400

    .env updated

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBDSdq2hw2_1FUcBI13_-u2yL6ZP16EfKo",
-  authDomain: "ai-pantry-tracker-4e577.firebaseapp.com",
-  projectId: "ai-pantry-tracker-4e577",
-  storageBucket: "ai-pantry-tracker-4e577.appspot.com",
-  messagingSenderId: "830023077374",
-  appId: "1:830023077374:web:987354be3d0ed8bb4684af",
-  measurementId: "G-HC197078DL"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MSG_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
